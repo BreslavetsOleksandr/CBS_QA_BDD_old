52c4ba19cff2f3c189edd2a5037e7167d1d24b6d
Oleksandr Breslavets <Oleksandr_Breslavets@epam.com>
2023-11-16 18:41:36 +0100

    TAF created

---
+++ CBSQA_BDD/src/test/java/cbsqa/stepDefinitions/TestRunner.java
@@ -0,0 +1,9 @@
+package cbsqa.stepDefinitions;
+
+//@Suite
+//@IncludeEngines("cucumber")
+//@SelectClasspathResource("features")
+//@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "cbsqa")
+public class TestRunner {
+
+}
